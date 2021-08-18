9c6a2edbf1d388621e2814ae58906c5670bb1b11
Duvier Reinosa <duvierreinosa@gmail.com>
2021-08-18 17:20:07 -0500

    get all notes and delete one note

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-const http = require('http')
+const { request } = require('express')
+const express = require('express')
+const app = express()
 
-const app = http.createServer
+let notes = [
+    {
+        "id": 1,
+        "content": "repasar los retos",
+        "date": "2019-05-30-30T18:39:34.091Z"
+    },
+    {
+        "id": 2,
+        "content": "matar los retos",
+        "date": "2019-05-30-30T18:39:34.091Z"
+    },
+    {
+        "id": 3,
+        "content": "repasar los retos",
+        "date": "2019-05-30-30T18:39:34.091Z"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>hello world</h1>')
+})
+
+app.get('/api/notes', (request, response) =>{
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) =>{
+    const id = Number(request.params.id)
+    const note = notes.find( note => note.id === id)
+    if (note) {
+        response.json(note) 
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) =>{
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Srver running on port ${PORT}`);
+})
