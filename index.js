198c544a1badfe8185dd4fb3f0cff646afb0c732
Duvier Reinosa <duvierreinosa@gmail.com>
2021-08-20 21:36:14 -0500

    add Heroku to this project

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.use((request, response) => {
     })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Srver running on port ${PORT}`)
 })
